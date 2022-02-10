ce2fe85019a138cf0958c6aeb30ac91a02be8d7a
Ryan Osmundson <rosmundson@outlook.com>
2022-02-10 13:03:36 -0700

    commit tree 39fa26b3427e542cc9952ee940173dda5e407221

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
 
+const random = Math.floor(Math.random() * 10)
+
+let num3 = 19
+let num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(1, 4, 6, 20, 8)
 // Write your code here
